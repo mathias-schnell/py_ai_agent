37292255aad27eb84da92c7d615872be79196f5f
mathias-schnell <mathiasschnell@gmail.com>
2025-06-26 22:23:13 -0700

    Added a "verbose" flag. Will print out the user's prompt and token counts when used, hiding them if not used.

--- main.py
+++ main.py
@@ -2,20 +2,27 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 if len(sys.argv) > 1:
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+    ]
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents=sys.argv[1]
+        contents=messages
     )
 else:
     raise Exception("no input provided")
     sys.exit(1)
 
 print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if "--verbose" in sys.argv:
+    print(f"User prompt: {sys.argv[1]}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
