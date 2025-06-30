8b94f47f5562f4b7df323fb60d44feadeecd7eea
mathias-schnell <mathiasschnell@gmail.com>
2025-06-30 13:22:52 -0700

    Updating to use UV as virtual environment, meaning some stuff is not required anymore. Updating main.py to setup AI system prompt.

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from google import genai
 from google.genai import types
 
 load_dotenv()
+
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
+model_name = "gemini-2.0-flash-001"
+system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
 
 if len(sys.argv) > 1:
     messages = [
@@ -14,8 +17,9 @@ if len(sys.argv) > 1:
     ]
 
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', 
-        contents=messages
+        model=model_name,
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
 else:
     raise Exception("no input provided")
