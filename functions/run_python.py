532462cfe720c7e23ce20775f4c5945627e46773
mathias-schnell <mathiasschnell@gmail.com>
2025-06-29 20:06:10 -0700

    Added new functions to help with running python scripts.

---
+++ functions/run_python.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import subprocess
+import sys
+
+def run_python_file(working_directory, file_path):
+    working_directory = Path(working_directory).resolve()
+    target_path = (working_directory / file_path).resolve()
+    result_str = ""
+
+    # Prevent access outside the working directory
+    if working_directory not in target_path.parents and working_directory != target_path:
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+
+    if not target_path.exists():
+        return f'Error: File "{file_path}" not found.'
+    
+    if target_path.suffix.lower() != ".py":
+        return f'Error: "{file_path}" is not a Python file.'
+    
+    try:
+        result = subprocess.run([sys.executable, str(target_path)], capture_output=True, text=True, timeout=30)
+
+        if result.stdout == '' and result.stderr == '':
+            result_str += "No output produced.\n"
+        else:
+            result_str += "STDOUT:" + result.stdout + "\n"
+            result_str += "STDERR:" + result.stderr + "\n"
+        if result.returncode != 0:
+            result_str += "Process exited with code " + str(result.returncode) + "\n"
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
+    
+    return result_str
